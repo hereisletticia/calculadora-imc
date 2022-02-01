7141b58f2f93f641dae63cf91f23f020099c2696
hereisletticia <hereisletticia@outlook.com>
2022-02-01 16:16:34 -0300

    commit dois, atualização

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-//html goes here
+import {useState} from 'react'
+import './app.css';
+
 export default function App(){
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+
+  function calculoIMC(){
+    alert("clcik")
+  }
+
   return(
-    <div classNome="app">
+    <div className='app'>
       <h1>Calculadora IMC</h1>
       <span>Vamos calcular seu IMC</span>
 
-      <div classNome="area-input">
+      <div className='area-input'>
         <input 
           type="text" 
           placeholder="Peso (kg) ex: 90"
+          value={weight}
+          onChange={ (e) => setWeight(e.target.value)}         
         />
         <input 
           type="text" 
           placeholder="Peso (cm) ex: 180"
+          value={height}
+          onChange={ (e) => setHeight(e.target.value)}          
         />
 
-        <button>
+        <button onClick={calculoIMC}>
           Calcular
         </button>
     </div>
+
       <h2>Seu IMC foi 25, você está abaixo do peso.</h2>
     </div>    
   )
