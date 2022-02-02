b533aa64006cf147417c33ba163a609790e4b495
hereisletticia <hereisletticia@outlook.com>
2022-02-02 00:46:56 -0300

    terceiro commit, app pronto

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import './app.css';
 export default function App(){
   const [weight, setWeight] = useState('');
   const [height, setHeight] = useState('');
+  const [message, setMessage] = useState('');
 
   function calculoIMC(){
-    alert("clcik")
+    const alt = height/100;
+    const imc = weight/(alt*alt);
+
+    if (imc < 18.6) {
+      setMessage("Você está abaixo do peso. " + " Seu IMC é: " + imc.toFixed(1));      
+    }else if (imc >= 18.6 && imc < 24.9) {
+      setMessage("Peso ideal." + " Seu IMC é: " + imc.toFixed(1));      
+    }else if (imc >= 24.9 && imc < 34.9) {
+      setMessage("Você está um pouco acima do peso. " + " Seu IMC é: " + imc.toFixed(1));
+    }else if (imc > 34.9) {
+      setMessage("Cuidado, OBESIDADE!" + " Seu IMC é: " + imc.toFixed(1));
+    }
   }
 
   return(
@@ -23,7 +35,7 @@ export default function App(){
         />
         <input 
           type="text" 
-          placeholder="Peso (cm) ex: 180"
+          placeholder="Altura (cm) ex: 180"
           value={height}
           onChange={ (e) => setHeight(e.target.value)}          
         />
@@ -33,7 +45,7 @@ export default function App(){
         </button>
     </div>
 
-      <h2>Seu IMC foi 25, você está abaixo do peso.</h2>
+      <h2>{message}</h2>
     </div>    
   )
 }
